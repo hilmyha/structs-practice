2c9b48611b5366b248102abdbf5ffca25d14b500
hilmyha <hilmyahmadhaidar@gmail.com>
2026-02-11 17:31:44 +0700

    feat: get user input, note var data

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Note taker")
+	title, content, err := getNoteData()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(title)
+	fmt.Println(content)
+}
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInput("Title: ")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	content, err := getUserInput("Content: ")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return title, content, nil
+}
+
+func getUserInput(prompt string) (string, error) {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scanln(&value)
+
+	if value == "" {
+		return "", errors.New("Value cannot be empty")
+	}
+	
+	return value, nil
 }
